b8336e2d0f58bc8476105924b9e7469b1975526d
Jozef Lelic <jozef@salsitasoft.com>
2022-03-07 00:48:02 +0100

    Rework CSV loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.(csv|tsv)$/i,
+            use: ['csv-loader'],
+          }
+        ]
+      }
+    }
+  }
 }
 
 module.exports = nextConfig
--- pages/api/verb-database.js
+++ pages/api/verb-database.js
@@ -2,33 +2,46 @@ import { EOL } from 'os'
 import * as fs from 'fs'
 import { parse } from 'fast-csv'
 
-let headers
+import VERBS_CSV from './verbs.csv'
+
+const headers = VERBS_CSV[0]
 const verbs = []
+VERBS_CSV.forEach((row,i) => {
+    if(i == 0) {
+        return
+    }
+    const verb = {}
+    headers.forEach((header,j) => {
+        verb[header] = row[j]
+    })
+    verbs.push(verb)
+})
 let loaded = false
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 
-const readCsv = async () => {
-    if(verbs.length > 0) {
-        return verbs
-    }
-    console.log('Starting reading')
-    return new Promise((resolve, reject) => {
-        fs.createReadStream('pages/api/verbs.csv')
-            .pipe(parse({headers: true}))
-            .on('error', error => {
-                console.error(error)
-                reject()
-            })
-            .on('headers', row => headers = row)
-            .on('data', row => verbs.push(row))
-            .on('end', (rowCount) => {
-                resolve()
-                console.log('Finished reading')
-            });
-    })
-}
+
+// const readCsv = async () => {
+//     if(verbs.length > 0) {
+//         return verbs
+//     }
+//     console.log('Starting reading')
+//     return new Promise((resolve, reject) => {
+//         fs.createReadStream('pages/api/verbs.csv')
+//             .pipe(parse({headers: true}))
+//             .on('error', error => {
+//                 console.error(error)
+//                 reject()
+//             })
+//             .on('headers', row => headers = row)
+//             .on('data', row => verbs.push(row))
+//             .on('end', (rowCount) => {
+//                 resolve()
+//                 console.log('Finished reading')
+//             });
+//     })
+// }
 
 const getRandomSubarray = (arr, size) => {
     let shuffled = arr.slice(0), i = arr.length, temp, index;
@@ -44,6 +57,6 @@ const getRandomSubarray = (arr, size) => {
 
 
 export async function getVerbs (num) {
-    await readCsv()
+    // await readCsv()
     return getRandomSubarray(verbs, num)
 }
